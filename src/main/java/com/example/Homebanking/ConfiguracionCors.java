1b83f2070598bb4d0d473d49f87a8c252322fa60
Gianina Azcurra <gianinazcurra@hotmail.com>
2023-09-25 17:48:40 -0300

    Configuracion CORS lista

--- src/main/java/com/example/Homebanking/ConfiguracionCors.java
+++ src/main/java/com/example/Homebanking/ConfiguracionCors.java
@@ -1,13 +1,33 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.example.Homebanking;
 
 /**
  *
- * @author admin
+ * @author HP
  */
-public class ConfiguracionCors {
-    
+
+    import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ConfiguracionCors implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://127.0.0.1:5500")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Content-Type")
+            .allowCredentials(true);
+    }
 }
+
+    
+
+
