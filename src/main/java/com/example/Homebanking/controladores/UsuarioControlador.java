8bbc9fd6d8bb843895ccf451709a0f92dfaabcd0
“Fabiana <fabyross09@gmail.com>
2023-10-05 18:18:32 -0300

    modificacion en el Usuario eliminar para poder agarrarlo con fetch

--- src/main/java/com/example/Homebanking/controladores/UsuarioControlador.java
+++ src/main/java/com/example/Homebanking/controladores/UsuarioControlador.java
@@ -5,8 +5,6 @@
  */
 package com.example.Homebanking.controladores;
 
-
-
 import com.example.Homebanking.Entidades.Usuario;
 import com.example.Homebanking.Errores.ErrorServicio;
 import java.util.Map;
@@ -39,25 +37,23 @@ public class UsuarioControlador {
     @Autowired
     com.example.Homebanking.Servicios.UsuarioServicio uSer;
 
-   @GetMapping("/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Usuario> perfilUsuario(@PathVariable String id) {
-        
+
         Usuario usuario = uSer.BuscarPorId(id);
-       if (usuario != null) {
+        if (usuario != null) {
             return ResponseEntity.ok(usuario);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-
-    
     //Este metodo registra un usuario con sus datos basicos.
     @PostMapping("/crearUsuario")
-    public String CrearUsuario(ModelMap modelo, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String Email, @RequestParam String clave,@RequestParam String DNI) throws Exception {
+    public String CrearUsuario(ModelMap modelo, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String Email, @RequestParam String clave, @RequestParam String DNI) throws Exception {
         try {
-            uSer.crear(nombre, apellido, Email, clave,DNI);
-            
+            uSer.crear(nombre, apellido, Email, clave, DNI);
+
             modelo.put("exito", "usted se ha creado un usuario, y una cuenta en el Banco...,sus tarjetas de debito y credito le llegaran a la brevedad a su domicilio");
             return "usted se ha creado un usuario";
         } catch (Exception e) {
@@ -66,29 +62,26 @@ public class UsuarioControlador {
         }
 
     }
-    
+
     //Este metodo le agrega a el usuario que le pasemos por parametro, una cuenta y tarjetas de debito y credito.
     @PostMapping("/cargarCuentayTarjetas")
-    public String CargarCuentayTarjetas(ModelMap modelo, @RequestParam String Id,@RequestParam Double saldo, @RequestParam Integer clave) throws Exception{
-        uSer.cargarTarjetasyCuenta(Id,saldo, clave);//es el IdUsuario
+    public String CargarCuentayTarjetas(ModelMap modelo, @RequestParam String Id, @RequestParam Double saldo, @RequestParam Integer clave) throws Exception {
+        uSer.cargarTarjetasyCuenta(Id, saldo, clave);//es el IdUsuario
         return "se creo su cuenta y se cargaron sus tarjetas de debito y credito";
     }
-    
-    
-    
-    
+
     //@PathVariable= configurar variables dentro de los propios segmentos de la URL
     //HttpSession= puede almacenar los datos de la sesión en el servidor y acceder a los mismos
     @GetMapping("/modificarUsuario/{id}")
     public ResponseEntity<Usuario> modificarUsuario(@PathVariable String id, ModelMap modelo, HttpSession session) throws ErrorServicio {
-         Usuario usuario = uSer.BuscarPorId(id);
-       if (usuario != null) {
+        Usuario usuario = uSer.BuscarPorId(id);
+        if (usuario != null) {
             return ResponseEntity.ok(usuario);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
-        
+
 //        Usuario usuarioLogueado = (Usuario) session.getAttribute("usuariosession");
 //        if (uSer.BuscarPorId(id).getIdUsuario().equals(usuarioLogueado.getIdUsuario())) {
 //            modelo.addAttribute("usuario", uSer.BuscarPorId(id));
@@ -98,21 +91,19 @@ public class UsuarioControlador {
 //            return "redirect:/index";
 //        }
 //    }
-
-    
     //Modifica todos los datos del usuario
     @PutMapping("/modificarUsuario/{Id}")
     public void ModificarUsuario(ModelMap modelo, @PathVariable String Id, @ModelAttribute Usuario usuarioForm) throws Exception {
 //        try {
 //            Usuario usuarioLogueado = (Usuario) session.getAttribute("usuariosession");
 //        if (uSer.BuscarPorId(Id).getIdUsuario().equals(usuarioLogueado.getIdUsuario())){
-if(usuarioForm!=null){
-            uSer.modificarDatosPersonales(Id, usuarioForm.getNombre(), usuarioForm.getApellido(),usuarioForm.getEmail(),usuarioForm.getClave(),usuarioForm.getDNI());
-}else{
-    System.out.println("El controlador esta recibiendo un objeto nulo desde el Formulario");
-}
-            //session.setAttribute("usuariosession", uSer.BuscarPorId(Id));
-            //modelo.put("exito", "usted ha modificado sus datos con exito");
+        if (usuarioForm != null) {
+            uSer.modificarDatosPersonales(Id, usuarioForm.getNombre(), usuarioForm.getApellido(), usuarioForm.getEmail(), usuarioForm.getClave(), usuarioForm.getDNI());
+        } else {
+            System.out.println("El controlador esta recibiendo un objeto nulo desde el Formulario");
+        }
+        //session.setAttribute("usuariosession", uSer.BuscarPorId(Id));
+        //modelo.put("exito", "usted ha modificado sus datos con exito");
 //        } else {
 //            throw new ErrorServicio("No puedes modificar este perfil");
 //        }
@@ -123,13 +114,13 @@ if(usuarioForm!=null){
 //        }
 
     }
-    
+
     //En este metodo se cambia la contraseña,requiere un codigo que es enviado al email.
     @PutMapping("/modificarPass")
     public String CambiarContrasena(ModelMap modelo, @RequestParam Integer codigo, @RequestParam String claveNueva, @RequestParam String email) throws Exception {
         try {
             uSer.cambiarContraseña(codigo, claveNueva, email);
-            
+
             modelo.put("exito", "la contraseña ha sido modificada con exito");
             return "usted ha modificado su contraseña con exito";
         } catch (Exception e) {
@@ -138,60 +129,56 @@ if(usuarioForm!=null){
         }
 
     }
-    
+
     //Da de baja el usuario
-     @PatchMapping("/darDeBajaUsuario")
+    @PatchMapping("/darDeBajaUsuario")
     public String DarDeBajaUsuario(ModelMap modelo, @RequestParam String Id) throws Exception {
         try {
             uSer.darBaja(Id);
-            
+
             modelo.put("exito", "usted fue dado de baja");
             return "el usuario ha sido dado de baja";
         } catch (Exception e) {
 
             return e.getMessage();
         }
-       
 
     }
-    
-    //Elimina al usuario con todos sus datos de la BD
-    @DeleteMapping("/EliminarUsuario")
-    public String BorrarUsuario(ModelMap modelo, @RequestParam String Id) throws Exception {
-        try {
-            uSer.EliminarUsuario(Id);
-            
-            modelo.put("exito", "el usuario fue eliminado");
-            
-            return "usted fue eliminado de la base de datos";
-            
-        } catch (Exception e) {
 
-            return e.getMessage();
+    //Elimina al usuario con todos sus datos de la BD
+    @DeleteMapping("/EliminarUsuario/{Id}")
+    public void BorrarUsuario(ModelMap modelo, @PathVariable String Id) throws Exception {
+
+        if (Id != null) {
+            uSer.EliminarUsuario(Id);
+
+        } else {
+            System.out.println("El controlador esta recibiendo un objeto nulo");
+
         }
-}
-    
+    }
+
     //busca al usuario por DNI y lo trae con todos sus datos
-     @GetMapping("/BuscarUsuarioPorDNI")
-    public String BuscarUsuarioDNI(ModelMap modelo, @RequestParam String DNI)  {
+    @GetMapping("/BuscarUsuarioPorDNI")
+    public String BuscarUsuarioDNI(ModelMap modelo, @RequestParam String DNI) {
         return "El usuario es :  " + uSer.BuscarUsuarioPorDNI(DNI);
     }
-    
+
     //Busca al usuario por Apellido y lo trae con todos sus datos
     @GetMapping("/BuscarUsuarioPorApellido")
-    public String BuscarUsuarioPorApellido(ModelMap modelo, @RequestParam String Apellido)  {
+    public String BuscarUsuarioPorApellido(ModelMap modelo, @RequestParam String Apellido) {
         return "El usuario es :  " + uSer.BuscarUsuarioPorApellido(Apellido);
     }
-    
+
     //Busca al usuario por email y lo trae con todos sus datos
     @GetMapping("/BuscarUsuarioPorEmail")
-    public String BuscarUsuarioPorEmail(ModelMap modelo, @RequestParam String email)  {
+    public String BuscarUsuarioPorEmail(ModelMap modelo, @RequestParam String email) {
         return "El usuario es :  " + uSer.BucarUsuarioPorEmail(email);
     }
-    
+
     //Busca al usuario por la cuenta que le sea pasada por parametro
     @GetMapping("/BuscarUsuarioPorCuenta")
-    public String BuscarUsuarioPorCuenta(ModelMap modelo, @RequestParam Long IdCuenta)  {
+    public String BuscarUsuarioPorCuenta(ModelMap modelo, @RequestParam Long IdCuenta) {
         return "El usuario es :  " + uSer.BuscarPorCuenta(IdCuenta);
     }
 }
