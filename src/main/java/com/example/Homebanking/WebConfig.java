b257e2c0563da0a60e57e75c981d169c869db318
Gianina Azcurra <gianinazcurra@hotmail.com>
2024-08-21 20:01:52 -0300

    Dejo Vista Lista
    Agrego clase WebConfig para rastrear archivos en static.

---
+++ src/main/java/com/example/Homebanking/WebConfig.java
@@ -0,0 +1,20 @@
+
+ 
+package com.example.Homebanking;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+         registry.addResourceHandler("/CSS/**")
+                .addResourceLocations("classpath:/static/CSS/");
+    }
+}
