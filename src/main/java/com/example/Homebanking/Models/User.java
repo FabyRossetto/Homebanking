23306e450d0d10892ad7cf0048410a53bcea9a18
Fabiana Rossetto <fabyross09@gmail.com>
2025-12-20 17:37:30 -0300

    fixing role

--- src/main/java/com/example/Homebanking/Models/User.java
+++ src/main/java/com/example/Homebanking/Models/User.java
@@ -78,7 +78,7 @@ public class User implements UserDetails{
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
      
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        return List.of(new SimpleGrantedAuthority(this.role.toString()));
     }
 
     @Override
